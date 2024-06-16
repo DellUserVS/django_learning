043606c80a9272f53a0fcbc48a49e815e812d73c
DellUserVS <rounakk2011@gmail.com>
2024-06-16 22:43:05 +0530

    Many changes

---
+++ django-polls/django-polls/django_polls/views.py
@@ -0,0 +1,53 @@
+from django.db.models import F
+from django.http import HttpResponse
+from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+from django.template import loader
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return last 5 published questions (not including those set to be published in the future)"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+def vote(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+
+    except (KeyError, Choice.DoesNotExist):
+        template = loader.get_template('polls/detail.html')
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice",
+        }
+        return HttpResponse(template.render(context, request))
+
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
